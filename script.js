a1ea2409f7a523f13eeaa6d9fcd0c5305154ce39
hetshrisolanki122-sketch <hetshrisolanki122@gmail.com>
2025-09-29 15:26:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth Scroll to Projects
+function scrollToProjects() {
+  document.getElementById("projects").scrollIntoView({ behavior: "smooth" });
+}
+
+// Show Project Details (Interactive Pop-up)
+function showDetails(projectName) {
+  alert("More details about " + projectName + " will be added soon!");
+}
